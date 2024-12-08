73e3baea73f4eda36377b5e3b5e022a4b30e900c
KAWAKAMI Moeki <hi@moeki.org>
2024-12-08 11:37:27 +0900

    Update

--- init.lua
+++ init.lua
@@ -134,6 +134,51 @@ local plugins = {
 		end,
 	},
 	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		config = function()
+			vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
+			vim.keymap.set("n", "<leader>nr", ":Neotree reveal<CR>", {})
+			require("neo-tree").setup({
+				filesystem = {
+					follow_current_file = {
+						enabled = true,
+					},
+				},
+				buffers = {
+					follow_current_file = {
+						enabled = true, -- This will find and focus the file in the active buffer every time
+						--              -- the current file is changed while the tree is open.
+						leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+					},
+				},
+				window = {
+					mappings = {
+						["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+					},
+				},
+			})
+		end,
+	},
+	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NeoTreeNormal",
+					"NeoTreeNormalNC",
+					"NormalFloat",
+				},
+			})
+		end,
+	},
+	{
 		"lewis6991/gitsigns.nvim",
 		event = { "BufReadPre", "BufNewFile" },
 		config = function()
@@ -459,7 +504,6 @@ local builtin = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>")
-
 local configs = require("nvim-treesitter.configs")
 
 configs.setup({
@@ -470,4 +514,4 @@ configs.setup({
 })
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
-require('lualine').setup()
+require("lualine").setup()
